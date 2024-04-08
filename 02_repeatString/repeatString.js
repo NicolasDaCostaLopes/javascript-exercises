6d338f52a1e3bab2f604251dd6244ccb3d000696
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-04-08 16:02:51 +0200

    Do exercises 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let output;
+    if (num >= 0) {
+        output = string.repeat(num);
+    } else output = "ERROR";
+    return output;
 };
 
 // Do not edit below this line
