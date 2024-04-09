1207b2a8ec334b96e129185cea0da53b2315b574
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-04-09 15:18:59 +0200

    Do exercises 4 to 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let allArguments = [...arguments];
+    let output = allArguments[0];
+    let argumentsToRemove = allArguments.splice(1, allArguments.length - 1);
 
+    for (itemIndex = 0; itemIndex < output.length; itemIndex++) {
+        for (removeIndex = 0; removeIndex < argumentsToRemove.length; removeIndex++) {
+            if (output[itemIndex] === argumentsToRemove[removeIndex]) {
+                output.splice(itemIndex, 1);
+                itemIndex--;
+            }
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    let output = 0;
+    for (i = start; i <= end; i++) {
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
