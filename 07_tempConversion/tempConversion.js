5d999e4c7d3d7b2ee227c11c75f802b0e394318a
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-04-10 13:59:32 +0200

    Do exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (number) {
+  let output;
+  output = +((number - 32) * 5 / 9).toFixed(1);
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (number) {
+  let output;
+  output = +(number * 9 / 5 + 32).toFixed(1);
+  return output;
 };
 
 // Do not edit below this line
