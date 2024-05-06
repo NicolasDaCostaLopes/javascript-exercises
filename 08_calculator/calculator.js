02e10d8c39b05536cc640e371d5ac9e27474ef9e
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-05-06 15:40:25 +0200

    Solve Calculator.js exercice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length == 0) return 0
+  return array.reduce((accumulator, current) => {
+    return accumulator + current
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((accumulator, current) => {
+    return accumulator * current
+  })
 };
 
-const power = function() {
-	
+const power = function (a, power) {
+  return a ** power
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  value = null
+  if (num === 0) value = 1; else {
+    for (let i = num; i > 0; i--) {
+      if (i === num) {
+        value = i;
+      } else {
+        value *= i;
+      }
+
+    }
+  }
+  return value
 };
+
 
 // Do not edit below this line
 module.exports = {
