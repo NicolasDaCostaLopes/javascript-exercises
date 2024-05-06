0e6c1bc93899e199601584405c2ed5843a124f48
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-05-06 16:04:39 +0200

    Solve Palindromes.js exercice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+let palindromes = function (word) {
+    let word1 = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "").toLowerCase();
+    let word2 = word1.split("").reverse().join("")
+    return word1 == word2;
 };
 
 // Do not edit below this line
