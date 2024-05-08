cfaba7a28a80bc7b73de3b9786c3966e8d8394a9
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-05-08 20:22:56 +0200

    Solve FindTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    return array.reduce((accumulator, person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        if (Object.keys(accumulator).length === 0) {
+            return person
+        } else {
+            if (person.yearOfDeath - person.yearOfBirth > accumulator.yearOfDeath - accumulator.yearOfBirth) {
+                return person
+            }
+            return accumulator
+        }
 
+    }, {})
 };
 
 // Do not edit below this line
