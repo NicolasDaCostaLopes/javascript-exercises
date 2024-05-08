245a4b71375430a54452b545595065b94fe5ee3d
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-05-08 19:52:41 +0200

    Solve Fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (wantedIndex) {
+    wantedIndex = +wantedIndex
+    if (wantedIndex < 0) return "OOPS"
+    let fib = [0, 1]
+    for (let i = 2; i - 1 < wantedIndex; i++) {
+        fib.push(fib[i - 2] + fib[i - 1]);
+    }
+    return fib[wantedIndex];
 };
 
 // Do not edit below this line
